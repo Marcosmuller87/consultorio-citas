7be77fc33dfcf7bcde421b0ff3b5f75bfb5b5a90
Marcos Muller <marcos.muller@magmati.com.br>
2023-02-03 11:25:29 -0300

    App Consultorio finalizada

--- src/app/components/crear-cita/crear-cita.component.ts
+++ src/app/components/crear-cita/crear-cita.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-crear-cita',
@@ -6,10 +6,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./crear-cita.component.css']
 })
 export class CrearCitaComponent implements OnInit {
+  nombre = '';
+  fecha = '';
+  hora = '';
+  sintomas = '';
+
+  formularioIncorrecto = false;
+  @Output() nuevaCita = new EventEmitter<any>();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  agregarCita() {
+    if(this.nombre == '' || this.fecha == '' || this.hora == '' || this.sintomas == ''){
+      this.formularioIncorrecto = true;
+      return;
+    } 
+    this.formularioIncorrecto = false;
+
+    // Creamos objeto para enviarselo al padre
+    const CITA = {
+      nombre: this.nombre,
+      fecha: this.fecha,
+      hora: this.hora,
+      sintomas: this.sintomas
+    }
+    console.log(CITA);
+    this.nuevaCita.emit(CITA);
+    this.resetCampos();
+  }
+
+  resetCampos() {
+    this.nombre = '';
+    this.fecha = '';
+    this.hora = '';
+    this.sintomas = '';
+  }
+
 }
+
